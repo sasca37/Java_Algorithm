048425cd3b960aa6132b05ed4671338f5ffb81d0
Jung JaeWook <sasca37@naver.com>
2022-02-15 14:19:05 +0900

    [Silver I] Title: 도영이가 만든 맛있는 음식, Time: 76 ms, Memory: 11536 KB -BaekjoonHub

---
+++ 백준/Silver/2961.도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static long answer = Integer.MAX_VALUE;
+    static int N, arr[][];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N][2];
+        for (int i=0; i<N; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+        binaryCounting();
+        System.out.println(answer);
+    }
+
+    private static void binaryCounting() {
+        int totalBit = 1<<N;
+        long a; // 신맛
+        long b; // 쓴맛
+        boolean chk;
+        for (int flag = 0; flag < totalBit; flag++) {
+            a = 1;
+            b = 0;
+            for (int i=0; i<N; i++) {
+                if ( (flag & 1 << i) != 0) {
+                    a *= arr[i][0];
+                    b += arr[i][1];
+                }
+            }
+            if(flag != 0)answer = Math.min(answer, Math.abs(a-b));
+        }
+    }
+}

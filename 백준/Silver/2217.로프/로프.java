ad6d47cc012b0ff48de9bb20be54cdd4e72d12d5
Jung JaeWook <sasca37@naver.com>
2022-02-16 09:20:43 +0900

    [Silver IV] Title: 로프, Time: 280 ms, Memory: 25996 KB -BaekjoonHub

---
+++ 백준/Silver/2217.로프/로프.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int answer = 0;
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) arr[i] = Integer.parseInt(br.readLine());
+        Arrays.sort(arr);
+        for (int i=0; i<N; i++) {
+            answer = Math.max(answer, arr[i] * (N-i));
+        }
+        System.out.println(answer);
+    }
+}

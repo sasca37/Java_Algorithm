f1f3ca2e4758e6df040064602341237fb2b8f23f
Jung JaeWook <sasca37@naver.com>
2022-02-17 21:13:25 +0900

    [Gold IV] Title: 알파벳, Time: 1020 ms, Memory: 12400 KB -BaekjoonHub

--- 백준/Gold/1987.알파벳/알파벳.java
+++ 백준/Gold/1987.알파벳/알파벳.java
@@ -4,53 +4,41 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-
     static int R, C;
-    static int[][] map;
-    static boolean[] visit = new boolean[26];
-    static int[] dx = { -1, 1, 0, 0 };
-    static int[] dy = { 0, 0, -1, 1 };
-    static int ans = 0;
-
-    public static void dfs(int x, int y, int count) {
-        if (visit[map[x][y]]) { // 0,0에 저장된 알파벳이 이미 한번 방문한 알파벳이라면,
-            ans = Math.max(ans, count); // 정답을 갱신해준다.
-            return; // 조건에 만족하므로 리턴.
-        } else {
-            visit[map[x][y]] = true;
-            for (int i = 0; i < 4; i++) {
-                int cx = x + dx[i];
-                int cy = y + dy[i];
-
-                if (cx >= 0 && cy >= 0 && cx < R && cy < C) {
-                    dfs(cx, cy, count + 1);
-                }
-
-            }
-
-            visit[map[x][y]] = false;
-
-        }
-    }
-
+    static int board[][];
+    static int answer = 0;
+    static int[] visited = new int[26];
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         R = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
-        map = new int[R][C];
+        board = new int[R][C];
         for (int i = 0; i < R; i++) {
-            String str = br.readLine();
+            String word = br.readLine();
             for (int j = 0; j < C; j++) {
-                map[i][j] = str.charAt(j) - 'A';
+                board[i][j] = word.charAt(j)-'A';
             }
         }
+        backtracking(0,0,0);
+        System.out.println(answer);
+    }
 
-        dfs(0, 0, 0);
-        // (0,0)부터 시작하며, 현재 이동한 위치는 0회
+    private static void backtracking(int x, int y,int cnt){
+        if (visited[board[x][y]] == 1) {
+            answer = Math.max(answer, cnt);
+            return;
+        }
 
-        System.out.println(ans);
+        visited[board[x][y]] = 1;
+        for (int i=0; i<4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (nx < 0 || ny < 0 || nx > R-1 || ny > C-1) continue;
+            backtracking(nx,ny,cnt+1);
+        }
+        visited[board[x][y]] = 0;
     }
 }

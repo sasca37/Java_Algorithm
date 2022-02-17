0e1d05793d964e5aa6683b8f6cd1cca52639ceb3
Jung JaeWook <sasca37@naver.com>
2022-02-17 10:19:29 +0900

    [Gold IV] Title: 벽 부수고 이동하기, Time: 524 ms, Memory: 138064 KB -BaekjoonHub

--- 백준/Gold/2206.벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206.벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -1,86 +1,72 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
- 
 public class Main {
- 
-    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    private static int N, M;
-    private static int[][] map;
-    private static int[][] visited;
-    private static int[] dx = {0, 0, -1, 1};
-    private static int[] dy = {-1, 1, 0, 0};
- 
-    static class Point {
-        int x, y, distance;
-        int drill; // 공사 횟수
- 
-        public Point(int x, int y, int distance, int drill) {
-            this.x = x;
-            this.y = y;
-            this.distance = distance;
-            this.drill = drill;
-        }
-    }
- 
+    static int N,M, board[][], visited[][];
+    static int dx[] = {0,0, -1,1};
+    static int dy[] = {-1,1, 0,0};
+    static int answer = -1;
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
- 
-        map = new int[N][M];
+        board = new int[N][M];
         visited = new int[N][M];
- 
         for (int i = 0; i < N; i++) {
-            String str = br.readLine();
+            char arr[] = br.readLine().toCharArray();
             for (int j = 0; j < M; j++) {
-                map[i][j] = Integer.parseInt(String.valueOf(str.charAt(j)));
+                board[i][j] = arr[j]-'0';
                 visited[i][j] = Integer.MAX_VALUE;
             }
         }
- 
-        int ans = bfs(0, 0);
-        bw.write(ans + "\n");
- 
-        bw.flush();
-        bw.close();
-        br.close();
+        bfs(0,0);
+        System.out.println(answer);
     }
- 
-    private static int bfs(int x, int y) {
-        Queue<Point> q = new LinkedList<>();
-        q.add(new Point(x, y, 1, 0)); // (1, 1)에서 시작
-        visited[y][x] = 0; // 처음 공사 횟수
- 
-        while (!q.isEmpty()) {
-            Point point = q.poll();
- 
-            // 도착지점을 만났다면 종료한다.
-            if (point.x == M - 1 && point.y == N - 1)
-                return point.distance;
- 
+
+    private static void bfs(int x, int y) {
+        Queue<Point> queue = new LinkedList<>();
+        queue.add(new Point(x,y,1,0));
+        visited[x][y] = 0;
+
+        while (!queue.isEmpty()) {
+            Point p = queue.poll();
+            if (p.x == N-1 && p.y == M-1) {
+                answer = p.cnt;
+                return;
+            }
             for (int i = 0; i < 4; i++) {
-                int nx = point.x + dx[i];
-                int ny = point.y + dy[i];
- 
-                if (nx >= 0 && nx < M && ny >= 0 && ny < N) {
-                    if (visited[ny][nx] > point.drill) {
-                        if (map[ny][nx] == 0) { // 벽이 아닐 때
-                            q.add(new Point(nx, ny, point.distance + 1, point.drill));
-                            visited[ny][nx] = point.drill;
-                        } else { // 벽일 때
-                            if (point.drill == 0) { // 지금까지 벽을 부순 횟수가 0이라면 
-                                q.add(new Point(nx, ny, point.distance + 1, point.drill + 1));
-                                visited[ny][nx] = point.drill + 1;
-                            }
-                        }
+                int nx = p.x + dx[i];
+                int ny = p.y + dy[i];
+                if(nx < 0 || ny < 0 || nx > N-1 || ny > M-1) continue;
+
+                if (visited[nx][ny] <= p.wall) continue;
+
+                if (board[nx][ny] == 0) {
+                    visited[nx][ny] = p.wall;
+                    queue.add(new Point(nx, ny, p.cnt + 1, p.wall));
+                }
+                else {
+                    if (p.wall == 0) {
+                        visited[nx][ny] = p.wall+1;
+                        queue.add(new Point(nx,ny,p.cnt+1, p.wall+1));
                     }
                 }
             }
         }
- 
-        return -1;
+    }
+}
+
+class Point {
+    int x,y, cnt, wall;
+
+    public Point(int x, int y, int cnt, int wall) {
+        this.x = x;
+        this.y = y;
+        this.cnt = cnt;
+        this.wall = wall;
     }
 }

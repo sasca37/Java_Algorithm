a15638aa2e9bb56e335c09d7c60aa724fb326573
Jung JaeWook <sasca37@naver.com>
2022-05-20 20:03:33 +0900

    [Silver I] Title: 효율적인 해킹, Time: 7816 ms, Memory: 304788 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
@@ -1,70 +1,52 @@
-import java.io.*;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
-
-    private static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-    private static boolean[] visited;
-    private static int[] result;
-
+    static int N, M, max, answer[];
+    static boolean[] visited;
+    static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        String[] nm = br.readLine().split(" ");
-
-        int n = Integer.parseInt(nm[0]);
-        int m = Integer.parseInt(nm[1]);
-
-        for (int i = 0; i <= n; i++) {
-            graph.add(new ArrayList<>());
-        }
-
-        for (int i = 0; i < m; i++) {
-            String[] fromTo = br.readLine().split(" ");
-            int from = Integer.parseInt(fromTo[0]);
-            int to = Integer.parseInt(fromTo[1]);
-
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        answer = new int[N+1];
+        for (int i = 0; i <= N; i++) graph.add(new ArrayList<>());
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
             graph.get(from).add(to);
         }
-        result = new int[n + 1];
 
-        for (int i = 1; i <= n; i++) {
-            visited = new boolean[n + 1];
-            getMaxPen(i);
+        for (int i = 1; i <= N; i++) {
+            visited = new boolean[N+1];
+            visited[i] = true;
+            bfs(i);
         }
 
-        int max = 0;
-
-        for (int i = 1; i <= n; i++) {
-            max = Math.max(max, result[i]);
+        for (int i = 1; i <= N; i++) {
+            if (max < answer[i])  max = answer[i];
         }
-
-        for (int i = 1; i <= n; i++) {
-            if(result[i] == max) {
-                bw.write(i + " ");
-            }
+        for (int i = 1; i <= N; i++) {
+            if (answer[i] == max) sb.append(i).append(" ");
         }
-
-        bw.flush();
-        bw.close();
+        System.out.println(sb);
     }
 
-    private static void getMaxPen(int x) {
+    private static void bfs(int node) {
         Queue<Integer> queue = new LinkedList<>();
-
-        queue.add(x);
-        visited[x] = true;
-
-        while(!queue.isEmpty()) {
-            int front = queue.remove();
-            for(int value : graph.get(front)) {
-                if(!visited[value]) {
-                    visited[value] = true;
-                    queue.add(value);
-                    result[value]++;
+        queue.add(node);
+        while (!queue.isEmpty()) {
+            int tmp = queue.poll();
+            for (int next : graph.get(tmp)) {
+                if (!visited[next]) {
+                    visited[next] = true;
+                    answer[next]++;
+                    bfs(next);
                 }
             }
         }

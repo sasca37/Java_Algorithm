d151b33ee5b124eb3a92ecf534b4d83a197bdb40
Jung JaeWook <sasca37@naver.com>
2022-05-27 13:17:58 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.90 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,54 +1,43 @@
 import java.util.*;
+
 class Solution {
+
     public int solution(int[][] jobs) {
-        ArrayList<Disk> arr = new ArrayList<>();
-        for (int i = 0; i < jobs.length; i++) {
-            arr.add(new Disk(jobs[i][0], jobs[i][1]));
-        }
-
-        Collections.sort(arr, new Comparator<Disk>() {
-            @Override
-            public int compare(Disk o1, Disk o2) {
-                if(o1.startTime == o2.startTime) return o1.requiredTime - o2.requiredTime;
-                return o1.startTime - o2.startTime;
-            }
-        });
-
-        PriorityQueue<Disk> pq = new PriorityQueue<>(new Comparator<Disk>() {
-            @Override
-            public int compare(Disk o1, Disk o2) {
-                return o1.requiredTime - o2.requiredTime;
-            }
-        });
-
         int answer = 0;
-        int cnt = 0;
-        int now = 0;
+        Arrays.sort(jobs, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] - o2[0];
+            }
+        });
 
+        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[1] - o2[1];
+            }
+        });
+
+        int curEndTime = 0; // 현재 종료 시간
+
+        int idx = 0;
         int i = 0;
-        while (cnt < arr.size()) {
-            while (i < arr.size() && arr.get(i).startTime <= now) {
-                pq.add(arr.get(i++));
+
+        while (idx < jobs.length) {
+            while (i < jobs.length && jobs[i][0] <= curEndTime) {
+                pq.add(jobs[i++]);
             }
-            if (pq.isEmpty()) {
-                now = arr.get(i).startTime;
+            if(pq.isEmpty()) {
+                curEndTime = jobs[i][0];
             } else {
-                Disk tmp = pq.poll();
-                answer += tmp.requiredTime + now - tmp.startTime;
-                now += tmp.requiredTime;
-                cnt++;
+                int[] tmp = pq.poll();
+                answer += tmp[1] + curEndTime - tmp[0];
+                curEndTime += tmp[1];
+                idx++;
             }
         }
 
-        return answer / arr.size();
-    }
 
-    static class Disk{
-        int startTime, requiredTime;
-
-        public Disk(int startTime, int requiredTime) {
-            this.startTime = startTime;
-            this.requiredTime = requiredTime;
-        }
+        return answer/ jobs.length;
     }
 }

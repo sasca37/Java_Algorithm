26ec43d8920b6aae2fe8ead62bbca64baad1ad9e
Jung JaeWook <sasca37@naver.com>
2022-06-05 14:30:40 +0900

    [Silver IV] Title: 상근이의 여행, Time: 180 ms, Memory: 22908 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = null;
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 0; t < T; t++) {
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            for (int i=0; i<m; i++) br.readLine();
+            sb.append(n - 1).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
+

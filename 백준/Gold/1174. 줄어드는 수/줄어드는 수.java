34367c2f593e552e8c9d82ff9a15060e9905b926
Jung JaeWook <sasca37@naver.com>
2022-06-10 13:32:48 +0900

    [Gold V] Title: 줄어드는 수, Time: 84 ms, Memory: 11648 KB -BaekjoonHub

---
+++ 백준/Gold/1174. 줄어드는 수/줄어드는 수.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N;
+    static ArrayList<Long> answer = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        answer.add(-1L);
+        N = Integer.parseInt(br.readLine());
+        if (N > 1023) System.out.println(-1);
+        else {
+            for (int i = 0; i < 10; i++) {
+                dfs(i, 1);
+            }
+            Collections.sort(answer);
+            System.out.println(answer.get(N));
+        }
+    }
+
+    private static void dfs(long num, int level) {
+        if(level > 10) return;
+        answer.add(num);
+        for (int i = 0; i < num % 10; i++) {
+            dfs(num * 10 + i, level+1);
+        }
+    }
+}

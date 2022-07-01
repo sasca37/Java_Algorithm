87a7d5eff152f0cd996d25355570469dd2afc2ef
Jung JaeWook <sasca37@naver.com>
2022-07-01 10:13:14 +0900

    [Bronze IV] Title: 상근날드, Time: 76 ms, Memory: 11528 KB -BaekjoonHub

---
+++ 백준/Bronze/5543. 상근날드/상근날드.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+        int d = Integer.parseInt(br.readLine());
+        int e = Integer.parseInt(br.readLine());
+        int ans = Math.min(a, Math.min(b, c)) + Math.min(d, e) - 50;
+        System.out.println(ans);
+    }
+}

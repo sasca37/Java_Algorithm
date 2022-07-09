d2868192e548a13662ce32be309d6739bb2c7ad8
Jung JaeWook <sasca37@naver.com>
2022-07-10 06:19:49 +0900

    [Bronze V] Title: 웰컴, Time: 72 ms, Memory: 11332 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(".  .   .").append("\n");
+        sb.append("|  | _ | _. _ ._ _  _").append("\n");
+        sb.append("|/\\|(/.|(_.(_)[ | )(/.");
+        System.out.println(sb);
+    }
+}

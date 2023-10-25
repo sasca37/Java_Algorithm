208652c93f5f326ea807c1017ee10cfb2d644034
sasca37 <sasca37@naver.com>
2023-10-25 21:49:59 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 1.27 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Solution {
+
+    public int solution(int[][] jobs) {
+        int answer = 0;
+        Arrays.sort(jobs, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] - o2[0];
+            }
+        });
+
+PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
+
+        int curEndTime = 0; // 현재 종료 시간
+
+        int idx = 0;
+        int i = 0;
+
+        while (idx < jobs.length) {
+            while (i < jobs.length && jobs[i][0] <= curEndTime) {
+                pq.add(jobs[i++]);
+            }
+            if(pq.isEmpty()) {
+                curEndTime = jobs[i][0];
+            } else {
+                int[] tmp = pq.poll();
+                answer += tmp[1] + curEndTime - tmp[0];
+                curEndTime += tmp[1];
+                idx++;
+            }
+        }
+
+
+        return answer/ jobs.length;
+    }
+}
